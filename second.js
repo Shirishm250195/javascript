ba25df5191e3753dc6df50d7a6f7d9fca34b8aa7
shirishm2501 <shirishm@techhighway.co.in>
2024-05-25 12:18:22 +0530

    2 may

---
+++ second.js
@@ -0,0 +1,11 @@
+a=10;
+console.log('type of a',a,typeof(a));
+
+b='hello world';
+console.log('type of b',b,typeof(b));
+
+// c;
+// console.log('type of c',c,typeof(c));
+
+d=null;
+console.log('type of d',d,typeof(d));
---
+++ third.js
@@ -0,0 +1,30 @@
+// age=Number(prompt("enter age"))
+
+// if(age<5){
+//     $res = 5000 * (50/100);
+//     $res = 5000 - $res;
+//     console.log("Discounted Price",$res)
+// }
+// else if(age>=5 && age<=55){
+//     $res = 5000 * (30/100);
+//     $res = 5000 - $res;
+//     console.log("Discounted Price",$res)
+// }
+// else if(age>55){
+//     $res = 5000 * (25/100);
+//     $res = 5000 - $res;
+//     console.log("Discounted Price",$res)
+// }
+
+
+// result=no1+no2
+// console.log("result",result)
+ 
+// console.log("a>b",a>b)
+// console.log("a<b",a<b)
+// console.log("a!=b",a!=b)
+// console.log("a==b",a==b)
+// console.log("a===b",a===b)
+
+
+// input the age from prompt if users age is less than 5 provide 50% of discount on the ticket price is in between 5 and 55 then provide 30 % of discount on the ticket price if age is greater than 55 then provide discount of 25 % the ticket price is 5000
